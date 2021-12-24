4a35596765c5135819f36159bd00718c7bb86bb1
dedyfuadi92 <dedyfuadi92@gmail.com>
2021-12-24 20:07:14 +0800

    select data sudah oke

--- controllers/mahasiswas.js
+++ controllers/mahasiswas.js
@@ -1,11 +1,15 @@
 // import { v4 as uuidv4 } from 'uuid';
+import { ok } from '../res.js';
 import { conn } from '../koneksi.js';
 let mahasiswas = [];
 
 export const getAllMahasiswa = (req, res) => {
     mahasiswas = conn.query("SELECT * FROM mahasiswa", function (err, result, fields) {
-        if (err) throw err;
-        console.log(result);
+        if (err) {
+            conn.log(err);
+        } else {
+            ok(result, res);
+            console.log('Berhasil ambil data');
+        }
     });
-    res.send(mahasiswas);
 };
