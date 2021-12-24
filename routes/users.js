cfbd1df4d97593073a33cf98f127ee01caaf8cf8
dedyfuadi92 <dedyfuadi92@gmail.com>
2021-12-24 09:16:56 +0800

    first commit

---
+++ routes/users.js
@@ -0,0 +1,43 @@
+import express from "express";
+import { v4 as uuidv4 } from 'uuid';
+
+const router = express.Router();
+/* const users = [
+    {
+        firstName: "John",
+        lastName: "Doe",
+        age: 25
+    },
+    {
+        firstName: "John",
+        lastName: "Doe",
+        age: 24
+    }
+] */
+const users = [];
+router.get('/', (req, res) => {
+    // console.log(users);
+    /* res.send('Hello this is a users page generate by routes'); */
+    res.send(users);
+});
+router.post('/', (req, res) => {
+    // console.log('LOG POST ROUTES REACHED');
+    // console.log(req.body);
+    const user = req.body;
+    // const userId = uuidv4();
+    // const userWithId = { ...user, id: userId };
+    // users.push(user);
+    // users.push(userWithId);
+    users.push({ ...user, id: uuidv4() });
+    // res.send('SEND POST ROUTES REACHED');
+    res.send(`User with the username ${user.firstName} added to the database`);
+});
+router.get('/:id', (req, res) => {
+    // console.log(req.params);
+    const { id } = req.params;
+    const foundUser = users.find((user) => user.id === id);
+    // res.send('THE GET ID ROUTE');
+    // res.send(req.params);
+    res.send(foundUser);
+});
+export default router;
