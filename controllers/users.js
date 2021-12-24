d7c33e4f5b14acb97250df0f459fc77faf3019c7
dedyfuadi92 <dedyfuadi92@gmail.com>
2021-12-24 10:45:52 +0800

    membuat controller

---
+++ controllers/users.js
@@ -0,0 +1,35 @@
+import { v4 as uuidv4 } from 'uuid';
+let users = [];
+export const getAllUser = (req, res) => {
+    res.send(users);
+};
+export const createUser = (req, res) => {
+    const user = req.body;
+    users.push({ ...user, id: uuidv4() });
+    res.send(`User with the username ${user.firstName} added to the database`);
+};
+export const getOneUser = (req, res) => {
+    const { id } = req.params;
+    const foundUser = users.find((user) => user.id === id);
+    res.send(foundUser);
+};
+export const deleteOneUser = (req, res) => {
+    const { id } = req.params;
+    users = users.filter((user) => user.id !== id);
+    res.send(`User with id ${id} has been deleted from the array`);
+};
+export const updateOneUser = (req, res) => {
+    const { id } = req.params;
+    const { firstName, lastName, age } = req.body;
+    const user = users.find((user) => user.id === id);
+    if (firstName) {
+        user.firstName = firstName;
+    }
+    if (lastName) {
+        user.lastName = lastName;
+    }
+    if (age) {
+        user.age = age;
+    }
+    res.send(`User with id ${id} has been updated`);
+};
--- routes/users.js
+++ routes/users.js
@@ -1,63 +1,12 @@
 import express from "express";
-import { v4 as uuidv4 } from 'uuid';
+
+import { createUser, deleteOneUser, getAllUser, getOneUser, updateOneUser } from "../controllers/users.js";
 
 const router = express.Router();
-/* const users = [
-    {
-        firstName: "John",
-        lastName: "Doe",
-        age: 25
-    },
-    {
-        firstName: "John",
-        lastName: "Doe",
-        age: 24
-    }
-] */
-let users = [];
-router.get('/', (req, res) => {
-    // console.log(users);
-    /* res.send('Hello this is a users page generate by routes'); */
-    res.send(users);
-});
-router.post('/', (req, res) => {
-    // console.log('LOG POST ROUTES REACHED');
-    // console.log(req.body);
-    const user = req.body;
-    // const userId = uuidv4();
-    // const userWithId = { ...user, id: userId };
-    // users.push(user);
-    // users.push(userWithId);
-    users.push({ ...user, id: uuidv4() });
-    // res.send('SEND POST ROUTES REACHED');
-    res.send(`User with the username ${user.firstName} added to the database`);
-});
-router.get('/:id', (req, res) => {
-    // console.log(req.params);
-    const { id } = req.params;
-    const foundUser = users.find((user) => user.id === id);
-    // res.send('THE GET ID ROUTE');
-    // res.send(req.params);
-    res.send(foundUser);
-});
-router.delete('/:id', (req, res) => {
-    const { id } = req.params;
-    users = users.filter((user) => user.id !== id);
-    res.send(`User with id ${id} has been deleted from the array`);
-});
-router.patch('/:id', (req, res) => {
-    const { id } = req.params;
-    const { firstName, lastName, age } = req.body;
-    const user = users.find((user) => user.id === id);
-    if (firstName) {
-        user.firstName = firstName;
-    }
-    if (lastName) {
-        user.lastName = lastName;
-    }
-    if (age) {
-        user.age = age;
-    }
-    res.send(`User with id ${id} has been updated`);
-});
+
+router.get('/', getAllUser);
+router.post('/', createUser);
+router.get('/:id', getOneUser);
+router.delete('/:id', deleteOneUser);
+router.patch('/:id', updateOneUser);
 export default router;
