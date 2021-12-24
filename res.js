72de73df0498b480b662cd9d350932fdce0f3c7b
dedyfuadi92 <dedyfuadi92@gmail.com>
2021-12-24 21:02:30 +0800

    select data sudah oke lagi

--- res.js
+++ res.js
@@ -1,10 +1,5 @@
 'use strict';
 export function ok(values, res) {
-    /* var data = {
-        'status': 200,
-        'values': values
-    }; */
-    // res.json(data);
     res.json(values);
     res.end();
 }
