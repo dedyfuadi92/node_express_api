4000c827b2441a3965a14934d36f5c864c36807d
dedyfuadi92 <dedyfuadi92@gmail.com>
2021-12-25 10:47:27 +0800

    insert data oke

--- controllers/mahasiswas.js
+++ controllers/mahasiswas.js
@@ -13,3 +13,44 @@ export const getAllMahasiswa = (req, res) => {
         }
     });
 };
+export const createMahasiswa = (req, res) => {
+    var nim = req.body.nim;
+    var nama = req.body.nama;
+    var jurusan = req.body.jurusan;
+    conn.query("insert into mahasiswa (nim,nama,jurusan) values (?,?,?)",
+        [nim, nama, jurusan],
+        function (err, result, fields) {
+            if (err) {
+                conn.log(err);
+            } else {
+                ok(`Berhasil insert data nim ${nim}`, res);
+                console.log('Berhasil insert data');
+            }
+        }
+    );
+};
+/* export const getOneUser = (req, res) => {
+    const { id } = req.params;
+    const foundUser = users.find((user) => user.id === id);
+    res.send(foundUser);
+};
+export const deleteOneUser = (req, res) => {
+    const { id } = req.params;
+    users = users.filter((user) => user.id !== id);
+    res.send(`User with id ${id} has been deleted from the array`);
+};
+export const updateOneUser = (req, res) => {
+    const { id } = req.params;
+    const { firstName, lastName, age } = req.body;
+    const user = users.find((user) => user.id === id);
+    if (firstName) {
+        user.firstName = firstName;
+    }
+    if (lastName) {
+        user.lastName = lastName;
+    }
+    if (age) {
+        user.age = age;
+    }
+    res.send(`User with id ${id} has been updated`);
+}; */
--- routes/mahasiswas.js
+++ routes/mahasiswas.js
@@ -1,5 +1,9 @@
 import express from "express";
-import { getAllMahasiswa } from "../controllers/mahasiswas.js";
+import { getAllMahasiswa, createMahasiswa } from "../controllers/mahasiswas.js";
 const router = express.Router();
 router.get('/', getAllMahasiswa);
+router.post('/', createMahasiswa);
+// router.get('/:id', getOneMahasiswa);
+// router.delete('/:id', deleteOneMahasiswa);
+// router.patch('/:id', updateOneMahasiswa);
 export default router;
